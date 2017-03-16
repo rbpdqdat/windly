5ec528a9d6ebe2e45c631f3334d47068503ac0cf
Russell Bigley <wx8ball@gmail.com>
2017-03-16 14:19:05 -0600

    changing average_forecast for python3.4

--- code/average_forecast.py
+++ code/average_forecast.py
@@ -14,7 +14,7 @@ from sklearn.model_selection import train_test_split
 # Note: data has the following form:
 # [timestamp,u,v,ws,wd,energy]
 def load_data(filepath, gap, hours):
-  # print "Loading Data"
+  # print("Loading Data"
   data = defaultdict(list)
   historical = defaultdict(list)
 
@@ -50,12 +50,12 @@ def load_data(filepath, gap, hours):
   return data
 
 def split_data(data, windfarm_id, ratio):
-  print "Splitting Data"
+  print("Splitting Data")
   return train_test_split(data[windfarm_id][:,:-1], 
     data[windfarm_id][:,-1], test_size = ratio, random_state=0)
 
 def split_data_cont(data, windfarm_id, ratio):
-  print "Splitting Data Contiguous"
+  print("Splitting Data Contiguous")
   n = len(data[windfarm_id])
   b = int(ratio*n)
   x_train = data[windfarm_id][:b, 1:-1]
@@ -65,7 +65,7 @@ def split_data_cont(data, windfarm_id, ratio):
   return x_train, x_test, y_train, y_test
 
 def run_gbrt(x_train, x_test, y_train, y_test):
-  print "Training GBRT\n"
+  print("Training GBRT\n")
   gbrt = GradientBoostingRegressor(n_estimators = 700, learning_rate = 0.1, max_depth = 2, 
     random_state = 0, min_samples_split = 100, max_features = 'sqrt')
   gbrt.fit(x_train, y_train)
@@ -75,7 +75,7 @@ def run_gbrt(x_train, x_test, y_train, y_test):
   return mse_train, mse_test
 
 def run_gbrt_grid(x_train, x_test, y_train, y_test):
-  print "\nTraining GBRT"
+  print("\nTraining GBRT")
 
   params = {'n_estimators':[700], 'learning_rate':[0.1], 
     'max_depth':[2]}
@@ -83,30 +83,30 @@ def run_gbrt_grid(x_train, x_test, y_train, y_test):
   grid = GridSearchCV(gbrt, params, scoring = 'neg_mean_squared_error', n_jobs = 4)
   grid.fit(x_train, y_train)
 
-  print "MSE of Model Prediction"
-  print "Training MSE", mean_squared_error(y_train, grid.predict(x_train))
-  print "Test MSE", mean_squared_error(y_test, grid.predict(x_test))
+  print("MSE of Model Prediction")
+  print("Training MSE", mean_squared_error(y_train, grid.predict(x_train)))
+  print("Test MSE", mean_squared_error(y_test, grid.predict(x_test)))
 
-  print "MSE of Predicting Average"
-  print "Average MSE", mean_squared_error(y_test, [np.mean(y_train)] * len(y_test))
+  print("MSE of Predicting Average")
+  print("Average MSE", mean_squared_error(y_test, [np.mean(y_train)] * len(y_test)))
 
-  print(grid.best_params_)
+  print(grid.best_params_))
 
 def run_mlp(x_train, x_test, y_train, y_test):
-  print "\nTraining MLP"
+  print("\nTraining MLP")
   params = {'hidden_layer_sizes':[(100,), (20, 20,)], 'learning_rate_init':[0.1, 0.05], 'alpha':[0.001, 0.01]}
   mlp = MLPRegressor(random_state = 0, learning_rate = 'adaptive')
   grid = GridSearchCV(mlp, params, scoring = 'neg_mean_squared_error', n_jobs = 4)
   grid.fit(x_train, y_train)
 
-  print "MSE of Model Prediction"
-  print "Training MSE", mean_squared_error(y_train, grid.predict(x_train))
-  print "Test MSE", mean_squared_error(y_test, grid.predict(x_test))
+  print("MSE of Model Prediction")
+  print("Training MSE", mean_squared_error(y_train, grid.predict(x_train)))
+  print("Test MSE", mean_squared_error(y_test, grid.predict(x_test)))
 
-  print "MSE of Predicting Average"
-  print "Average MSE", mean_squared_error(y_test, [np.mean(y_train)] * len(y_test))
+  print("MSE of Predicting Average")
+  print("Average MSE", mean_squared_error(y_test, [np.mean(y_train)] * len(y_test)))
 
-  print grid.best_params_
+  print(grid.best_params_)
   
 
 def main():
